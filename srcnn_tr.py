b8bac9a569064d7d0a8499f7b2d4f51513fa7fe3
himazin331 <sion20022013@gmail.com>
2022-07-03 22:16:21 +0900

    Refactoring

--- srcnn_tr.py
+++ srcnn_tr.py
@@ -1,5 +1,6 @@
+from black import Any
 import tensorflow as tf
-import tensorflow.keras.layers as kl
+import tensorflow.keras.layers as L
 from tensorflow.python.keras import backend as K
 
 import cv2
@@ -9,19 +10,20 @@ import matplotlib.pyplot as plt
 
 import argparse as arg
 import os
-import sys
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # TFメッセージ非表示
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # TFメッセージ非表示
 
 
 # SRCNN
 class SRCNN(tf.keras.Model):
-    def __init__(self, h, w):
+    def __init__(self, h: int, w: int):
         super(SRCNN, self).__init__()
 
-        self.conv1 = kl.Conv2D(64, 3, padding='same', activation='relu', input_shape=(None, h, w, 3))
-        self.conv2 = kl.Conv2D(32, 3, padding='same', activation='relu')
-        self.conv3 = kl.Conv2D(3, 3, padding='same', activation='relu')
+        self.conv1 = L.Conv2D(
+            64, 3, padding="same", activation="relu", input_shape=(None, h, w, 3)
+        )
+        self.conv2 = L.Conv2D(32, 3, padding="same", activation="relu")
+        self.conv3 = L.Conv2D(3, 3, padding="same", activation="relu")
 
     def call(self, x):
         h1 = self.conv1(x)
@@ -33,15 +35,25 @@ class SRCNN(tf.keras.Model):
 
 # 学習
 class trainer(object):
-    def __init__(self, h, w):
+    def __init__(self, h: int, w: int):
         self.model = SRCNN(h, w)
-        
-        self.model.compile(optimizer=tf.keras.optimizers.Adam(),
-                            loss=tf.keras.losses.MeanSquaredError(),
-                            metrics=[self.psnr])
 
-    def train(self, lr_imgs, hr_imgs, out_path, batch_size, epochs):
+        self.model.compile(
+            optimizer=tf.keras.optimizers.Adam(),
+            loss=tf.keras.losses.MeanSquaredError(),
+            metrics=[self._psnr],
+        )
+
+    def train(
+        self,
+        lr_imgs: tf.Tensor,
+        hr_imgs: tf.Tensor,
+        out_path: str,
+        batch_size: int,
+        epochs: int,
+    ) -> tuple[Any, SRCNN]:  # ?[TypeHints] Any -> his: keras.callbacks.history
         # 学習
+        # ?[TypeHints] his: keras.callbacks.history
         his = self.model.fit(lr_imgs, hr_imgs, batch_size=batch_size, epochs=epochs)
 
         print("___Training finished\n\n")
@@ -54,39 +66,42 @@ class trainer(object):
         return his, self.model
 
     # PSNR(ピーク信号対雑音比)
-    def psnr(self, h3, hr_imgs):
-        return -10 * K.log(K.mean(K.flatten((h3 - hr_imgs))**2)) / np.log(10)
+    def _psnr(self, h3, hr_imgs: tf.Tensor):
+        return -10 * K.log(K.mean(K.flatten((h3 - hr_imgs)) ** 2)) / np.log(10)
 
 
 # データセット作成
-def create_dataset(data_dir, h, w, mag):
+def create_dataset(
+    data_dir: str, h: int, w: int, mag: int
+) -> tuple[tf.Tensor, tf.Tensor]:
     print("\n___Creating a dataset...")
-    
-    prc = ['/', '-', '\\', '|']
-    cnt = 0
+
+    prc: list[str] = ["/", "-", "\\", "|"]
+    cnt: int = 0
 
     # 画像データの個数
     print("Number of image in a directory: {}".format(len(os.listdir(data_dir))))
 
-    lr_imgs = []
-    hr_imgs = []
+    lr_imgs: tf.Tensor = []
+    hr_imgs: tf.Tensor = []
 
     for c in os.listdir(data_dir):
-        d = os.path.join(data_dir, c)
+        d: str = os.path.join(data_dir, c)
 
+        ext: str
         _, ext = os.path.splitext(c)
-        if ext.lower() == '.db':
+        if ext.lower() == ".db":
             continue
-        elif ext.lower() != '.bmp':
+        elif ext.lower() != ".bmp":
             continue
 
         # 読込、リサイズ(高解像画像)
-        img = cv2.imread(d)
+        img: np.ndarray = cv2.imread(d)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = cv2.resize(img, (w, h))
 
         # 低解像度画像
-        img_low = cv2.resize(img, (int(w / mag), int(h / mag)))
+        img_low: np.ndarray = cv2.resize(img, (int(w / mag), int(h / mag)))
         img_low = cv2.resize(img_low, (w, h))
 
         lr_imgs.append(img_low)
@@ -94,75 +109,90 @@ def create_dataset(data_dir, h, w, mag):
 
         cnt += 1
 
-        print("\rLoading a LR-images and HR-images...{}    ({} / {})".format(prc[cnt % 4], cnt, len(os.listdir(data_dir))), end='')
+        print(
+            "\rLoading a LR-images and HR-images...{}    ({} / {})".format(
+                prc[cnt % 4], cnt, len(os.listdir(data_dir))
+            ),
+            end="",
+        )
 
-    print("\rLoading a LR-images and HR-images...Done    ({} / {})".format(cnt, len(os.listdir(data_dir))), end='')
+    print(
+        "\rLoading a LR-images and HR-images...Done    ({} / {})".format(
+            cnt, len(os.listdir(data_dir))
+        ),
+        end="",
+    )
 
     # 正規化
     lr_imgs = tf.convert_to_tensor(lr_imgs, np.float32)
-    lr_imgs /= 255
+    lr_imgs = lr_imgs / 255
     hr_imgs = tf.convert_to_tensor(hr_imgs, np.float32)
-    hr_imgs /= 255
+    hr_imgs = hr_imgs / 255
 
     print("\n___Successfully completed\n")
     return lr_imgs, hr_imgs
 
 
 # PSNR, 損失値グラフ出力
-def graph_output(history):
+def graph_output(history):  # ?[TypeHints] history: keras.callbacks.history
     # PSNRグラフ
-    plt.plot(history.history['psnr'])
-    plt.title('Model PSNR')
-    plt.ylabel('PSNR')
-    plt.xlabel('Epoch')
-    plt.legend(['Train'], loc='upper left')
+    plt.plot(history.history["_psnr"])
+    plt.title("Model PSNR")
+    plt.ylabel("PSNR")
+    plt.xlabel("Epoch")
+    plt.legend(["Train"], loc="upper left")
     plt.show()
 
     # 損失値グラフ
-    plt.plot(history.history['loss'])
-    plt.title('Model loss')
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
-    plt.legend(['Train'], loc='upper left')
+    plt.plot(history.history["loss"])
+    plt.title("Model loss")
+    plt.ylabel("Loss")
+    plt.xlabel("Epoch")
+    plt.legend(["Train"], loc="upper left")
     plt.show()
 
 
 def main():
     # コマンドラインオプション作成
-    parser = arg.ArgumentParser(description='Super-resolution CNN training')
-    parser.add_argument('--data_dir', '-d', type=str, default=None,
-                        help='画像フォルダパスの指定(未指定ならエラー)')
-    parser.add_argument('--out', '-o', type=str,
-                        default=os.path.dirname(os.path.abspath(__file__)),
-                        help='パラメータの保存先指定(デフォルト値=./srcnn.h5')
-    parser.add_argument('--batch_size', '-b', type=int, default=32,
-                        help='ミニバッチサイズの指定(デフォルト値=32)')
-    parser.add_argument('--epoch', '-e', type=int, default=3000,
-                        help='学習回数の指定(デフォルト値=3000)')
-    parser.add_argument('--he', '-he', type=int, default=256,
-                        help='リサイズの高さ指定(デフォルト値=256)')
-    parser.add_argument('--wi', '-wi', type=int, default=256,
-                        help='リサイズの指定(デフォルト値=256)')
-    parser.add_argument('--mag', '-m', type=int, default=2,
-                        help='縮小倍率の指定(デフォルト値=2)')
+    parser = arg.ArgumentParser(description="Super-resolution CNN training")
+    parser.add_argument(
+        "--data_dir", "-d", type=str, default=None, help="画像フォルダパスの指定(未指定ならエラー)"
+    )
+    parser.add_argument(
+        "--out",
+        "-o",
+        type=str,
+        default=os.path.dirname(os.path.abspath(__file__)),
+        help="パラメータの保存先指定(デフォルト値=./srcnn.h5",
+    )
+    parser.add_argument(
+        "--batch_size", "-b", type=int, default=32, help="ミニバッチサイズの指定(デフォルト値=32)"
+    )
+    parser.add_argument(
+        "--epoch", "-e", type=int, default=3000, help="学習回数の指定(デフォルト値=3000)"
+    )
+    parser.add_argument(
+        "--he", "-he", type=int, default=256, help="リサイズの高さ指定(デフォルト値=256)"
+    )
+    parser.add_argument(
+        "--wi", "-wi", type=int, default=256, help="リサイズの指定(デフォルト値=256)"
+    )
+    parser.add_argument("--mag", "-m", type=int, default=2, help="縮小倍率の指定(デフォルト値=2)")
     args = parser.parse_args()
 
     # 画像フォルダパス未指定->例外
     if args.data_dir is None:
-        print("\nException: Folder not specified.\n")
-        sys.exit()
+        raise ValueError("Folder not specified.")
     # 存在しない画像フォルダ指定時->例外
     if os.path.exists(args.data_dir) is False:
-        print("\nException: Folder \"{}\" is not found.\n".format(args.data_dir))
-        sys.exit()
-    # 幅高さ、縮小倍率いずれかに0が入力された時->例外
-    if args.he == 0 or args.wi == 0 or args.mag == 0:
-        print("\nException: Invalid value has been entered.\n")
-        sys.exit()
+        raise ValueError("Folder {} is not found.".format(args.data_dir))
+    # 幅高さ、縮小倍率いずれかに0以下が入力された時->例外
+    if args.he <= 0 or args.wi <= 0 or args.mag <= 0:
+        raise ValueError("Invalid value has been entered.")
 
     # 出力フォルダの作成(フォルダが存在する場合は作成しない)
     os.makedirs(args.out, exist_ok=True)
-    out_path = os.path.join(args.out, "srcnn.h5")
+    out_path: str = os.path.join(args.out, "srcnn.h5")
 
     # 設定情報出力
     print("=== Setting information ===")
@@ -177,16 +207,27 @@ def main():
     print("===========================\n")
 
     # データセット作成
+    lr_imgs: tf.Tensor
+    lr_imgs: tf.Tensor
     lr_imgs, hr_imgs = create_dataset(args.data_dir, args.he, args.wi, args.mag)
 
     # 学習開始
     print("___Start training...")
     Trainer = trainer(args.he, args.wi)
-    his, model = Trainer.train(lr_imgs, hr_imgs, out_path=out_path, batch_size=args.batch_size, epochs=args.epoch)
+
+    # ?[TypeHints] his: keras.callbacks.history
+    model: SRCNN
+    his, model = Trainer.train(
+        lr_imgs,
+        hr_imgs,
+        out_path=out_path,
+        batch_size=args.batch_size,
+        epochs=args.epoch,
+    )
 
     # PSNR, 損失値グラフ出力、保存
     graph_output(his)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
